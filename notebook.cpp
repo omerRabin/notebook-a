bf35df51bf6f201b8df48bade9529663f686da18
omerRabin <73130830+omerRabin@users.noreply.github.com>
2022-03-20 20:32:23 +0200

    Create notebook.cpp

---
+++ notebook.cpp
@@ -0,0 +1,25 @@
+#include "Notebook.hpp"
+#include <iostream>
+/*
+    Notebook.cpp.
+    Author: Omer Rabin.
+*/
+using namespace std;
+
+namespace ariel {
+    void Notebook::write(uint page, uint row, uint col, Direction direction, string message) 
+    {
+        return;
+    }
+
+    string Notebook::read(uint page, uint row, uint col, Direction direction, uint length){
+        return "";
+    }
+        
+    void Notebook::show(uint page) {
+        return;
+    }
+    void Notebook::erase(uint page, uint row, uint col, Direction direction, uint length){
+        return;
+    }
+}
